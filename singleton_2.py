d1f7d9cee43b4b55743eb892ef1088bc7cd78987
Eduardo Costa <educosta.costa@gmail.com>
2024-07-25 23:03:18 +0100

    singleton2

--- singleton_2.py
+++ singleton_2.py
@@ -9,15 +9,24 @@ Its use is almost always a design smell.)
 - Erich Gamma, em entrevista para infomIT
 http://www.informit.com/articles/article.aspx?p=1404056
 """
+def singleton(the_class):
+    instances = {}
 
-class AppSettings:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super().__new__(cls,*args, **kwargs)
-        return cls._instance
+    def get_class(*args, **kwargs):
+        if the_class not in instances:
+            instances[the_class] = the_class(*args, **kwargs)
+        return instances[the_class]
     
+    return get_class  
+
+@singleton
+class Teste:
+    def __init__(self) -> None:
+        print('Hello')  
+
+@singleton
+class AppSettings:
+      
     def __init__(self) -> None:
         self.tema = 'dark'
         self.font = '18px'
@@ -30,7 +39,8 @@ if __name__ == '__main__':
     ApS_2 = AppSettings()
     print(ApS_1.tema)
 
+    t1 = Teste()
+    t2 = Teste()
     
-    print(ApS_1)
-    print(ApS_2)
+   
 
