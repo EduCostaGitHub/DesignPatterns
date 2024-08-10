d3511ab7131b1858ed013690bb95866159b24969
Eduardo Costa <educosta.costa@gmail.com>
2024-08-10 22:21:10 +0100

    COR

---
+++ chain_of_responsability_1.py
@@ -0,0 +1,24 @@
+"""
+Chain of responsability (COR) é um padrão comportamental
+que tem a intenção de evitar o acoplamento do remetente de 
+um solicitação ao seu receptor, ao dar a mais de um objecto
+a oportunidade de tratar a solicitação.
+Encadear os objectod receptores passando a solicitação 
+ao longo da cadeia até que um objecto a trate.
+
+
+"""
+from __future__ import annotations
+from monostate_2 import StringReprMixin
+from typing import List, Dict, Tuple
+from abc import ABC, abstractmethod
+
+
+if __name__== "__main__":
+    pass
+    
+
+
+
+
+    
