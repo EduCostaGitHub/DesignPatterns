a6db168fd786680753d4d987aeecc8452ecb2dbd
Eduardo Costa <educosta.costa@gmail.com>
2024-08-19 18:01:18 +0100

    flyweight

--- flyweight.py
+++ flyweight.py
@@ -29,15 +29,44 @@ esse estado deve estar dentro do objecto flyweight;
 esse estado pode ser movido para fora do objecto 
 flyweight;
 
+Dicionário:
 
+Intrínseco - Que faz parte de ou que constitui a 
+essência, a natureza de algo: que é próprio de 
+algo: inerente.
 
+Extrínseco - Que não pertence à essência de algo:
+que é exterior.
 """
 
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List, Dict, Literal
 
+class Client:
+    """Context"""
+    def __init__(self, name : str) -> None:
+        self.name: str = name
+        self._addresses: List[Address] =[]
 
+        # Extrinsic address data
+        self._address_number: str
+        self._address_detail: str
+
+    def add_address(self, address : Address) -> None:
+        self._addresses.append(address)
+
+    def list_addreses(self) -> None:
+        for address in self._addresses:
+            address.show_address(self._address_number, self._address_detail)
+
+class Address:
+    """Flyweight"""
+    def __init__(self) -> None:
+        pass
+
+    def show_address(self, address_number:str, address_detail:str) -> None:
+        pass
 
 if __name__ == "__main__":
     pass
